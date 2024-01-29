7ad239c8914f2e61bde08ebda71820bc73a70853
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-29 19:17:26 +0330

    #

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-const users = new Set()
-function isRegister(username){
-if()
+const users = new Set();
+function isRegister(username) {
+  if (users.has(username)) {
+    console.log("invalid username");
+    return;
+  } else {
+    users.add(username);
+    console.log("registered successfully");
+    return;
+  }
 }
