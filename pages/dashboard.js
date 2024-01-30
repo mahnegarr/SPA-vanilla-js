0a725b416fd6ee8e98e9bc6df27dad067da88205
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-30 22:46:51 +0330

    #

---
+++ pages/dashboard.js
@@ -0,0 +1,5 @@
+const dashboard =()=>{
+    return `<h1>dashboard Page</h1>`
+}
+
+export default dashboard
---
+++ pages/posts.js
@@ -0,0 +1,5 @@
+const posts =()=>{
+    return `<h1>posts Page</h1>`
+}
+
+export default posts
