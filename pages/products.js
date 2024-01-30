7c5fbb4d911bfaec8d71655d2026c7519ba6adb5
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-30 22:46:24 +0330

    #

---
+++ pages/products.js
@@ -0,0 +1,5 @@
+const products =()=>{
+    return `<h1>Products Page</h1>`
+}
+
+export default products
--- script.js
+++ script.js
@@ -5,24 +5,33 @@ function router() {
     { path: "/products", view: () => console.log("products page") },
   ];
 
-  const potentialPaths = paths.map(item=>{
-    return{
-        route:item,
-        isMatch: location.pathname === item.path
-    }
-  })
+  const potentialPaths = paths.map((item) => {
+    return {
+      route: item,
+      isMatch: location.pathname === item.path,
+    };
+  });
 
-  let match = potentialPaths.find(route=>route.isMatch);
+  let match = potentialPaths.find((route) => route.isMatch);
 
-  if(!match){
-    match={
-        route: {path:"/not-found",view:()=>console.log("not found page")}
-    }
+  if (!match) {
+    match = {
+      route: { path: "/not-found", view: () => console.log("not found page") },
+    };
   }
 
   console.log(match.route.view());
 }
-
-document.addEventListener("DOMContentLoaded",()=>{
-    router()
-})
+function navigateTo(url) {
+  history.pushState(null, null, url);
+  router();
+}
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches([data - link])) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
