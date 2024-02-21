4e2d0f008e0deded9396eaa4be6b823a53f864fc
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-02-21 12:35:55 +0330

    #

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-const users = new Set();
-function isRegister(username) {
-  if (users.has(username)) {
-    console.log("invalid username");
-    return;
-  } else {
-    users.add(username);
-    console.log("registered successfully");
-    return;
-  }
-}
-const ads = new Set();
-function addAdvertise(title, username) {
-  if (!isRegister(username)) {
-    console.log("invalid username");
-    return;
-  }
-  if (ads.has(title)) {
-    console.log("invalid title");
-  }
+const userName = "mahnegar";
 
-  ads.add(title);
-  console.log("posted successfully");
-}
+const getUser = () => {
+  "mahnegar is a developer";
+};
